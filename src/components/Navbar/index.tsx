fcc748949b95e9547ecbf7976e935c1bd05f6b57
Pedro Lucas <pdrolucas.dev@gmail.com>
2022-12-03 21:09:53 -0300

    feat: create navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,3 +1,37 @@
-export default function Navbar() {
-  return <h1>Navbar</h1>;
-}
+import Link from 'next/link';
+
+import { AppBar, Container, Link as MUILink, Toolbar } from '@mui/material';
+import { useState } from 'react';
+
+const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar position="fixed">
+      <Toolbar disableGutters>
+        <Container maxWidth="xl">
+          <Link href="/home" passHref>
+            <MUILink
+              variant="h6"
+              component="button"
+              underline="none"
+              color="white"
+            >
+              Logo
+            </MUILink>
+          </Link>
+        </Container>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
