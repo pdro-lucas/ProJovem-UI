16e1e9235990322ac5a1a05e23f4ae94a19603bd
Pedro Lucas <pdrolucas.dev@gmail.com>
2022-12-04 01:23:39 -0300

    feat: create initial styles of navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,36 +1,67 @@
 import Link from 'next/link';
 
-import { AppBar, Container, Link as MUILink, Toolbar } from '@mui/material';
 import { useState } from 'react';
 
+import {
+  AppBar,
+  Container,
+  Typography,
+  Toolbar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import { AppleLogo, List } from 'phosphor-react';
+import MobileNavbar from './MobileNavbar';
+
 const Navbar = () => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [mobileOpen, setMobileOpen] = useState(false);
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(null);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
   };
 
   return (
-    <AppBar position="fixed">
-      <Toolbar disableGutters>
-        <Container maxWidth="xl">
-          <Link href="/home" passHref>
-            <MUILink
-              variant="h6"
-              component="button"
-              underline="none"
-              color="white"
+    <>
+      <AppBar position="fixed" component="nav">
+        <Toolbar disableGutters>
+          <Container maxWidth="xl">
+            <Link
+              href="/home"
+              style={{
+                textDecoration: 'none',
+                color: '#FFFFFF',
+              }}
             >
-              Logo
-            </MUILink>
-          </Link>
-        </Container>
-      </Toolbar>
-    </AppBar>
+              <Box
+                sx={{
+                  display: { xs: 'none', md: 'flex' },
+                  alignItems: 'center',
+                  gap: '4px',
+                }}
+              >
+                <AppleLogo size={24} />
+                <Typography variant="h6">Logo</Typography>
+              </Box>
+            </Link>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer menu"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{
+                mr: 2,
+                display: { md: 'none' },
+              }}
+            >
+              <List size={32} />
+            </IconButton>
+          </Container>
+        </Toolbar>
+      </AppBar>
+      <MobileNavbar open={mobileOpen} handleDrawerToggle={handleDrawerToggle} />
+    </>
   );
 };
 
