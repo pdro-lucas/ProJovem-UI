bb48c00a39e68545c02c729aed6cc4b58acfb4df
Pedro Lucas <pdrolucas.dev@gmail.com>
2022-12-23 16:43:36 -0300

    feat: adiconado botao dropdown na barra de navegacao
    Adicionado botao dropdown para navegar nas subpaginas de uma categoria. O dropdown ainda precisa ser
    estilizado e a barra de navegacao precisa ser responsiva, mas a implementacao base esta pronta

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-import { useState } from 'react';
+import { ReactElement, useState } from 'react';
 
 import {
   AppBar,
@@ -9,85 +9,136 @@ import {
   Toolbar,
   Box,
   IconButton,
+  List,
+  ListItemButton,
+  ListItemText,
+  Collapse,
+  Link as MUILink,
 } from '@mui/material';
-import { AppleLogo, House, List } from 'phosphor-react';
-import MobileNavbar from './MobileNavbar';
+import { AppleLogo, House, List as ListIcon } from 'phosphor-react';
 
+interface IItems {
+  label: string;
+  link: string;
+}
+interface INavItems {
+  id: number;
+  label: string;
+  link: string;
+  icon: ReactElement;
+  subPages?: IItems[];
+}
+
+// TODO: Atualizar o tema da aplicação e ver como o código abaixo pode ser melhorado
 const Navbar = () => {
-  const [mobileOpen, setMobileOpen] = useState(false);
+  const [navItemDropdownOpen, setNavItemDropdownOpen] = useState({});
 
-  const navItems = [
+  const navItems: INavItems[] = [
     {
       id: 1,
       label: 'Jovens',
+      link: '/home',
       icon: <House size={32} />,
-      items: {
-        label: 'Listar Todos',
-      },
+      subPages: [
+        {
+          label: 'Listar Todos',
+          link: '/listar_todos',
+        },
+      ],
     },
     {
       id: 2,
       label: 'Empresas',
+      link: '/empresas',
       icon: <House size={32} />,
-      items: {
-        label: 'Listar Todos',
-      },
+      subPages: [
+        {
+          label: 'Listar Todos',
+          link: '/listar_todos',
+        },
+        {
+          label: 'Listar Todos',
+          link: '/listar_todos',
+        },
+      ],
     },
     {
       id: 3,
       label: 'Teste',
+      link: '/teste',
       icon: <House size={32} />,
     },
   ];
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+  function handleShowDropdown(id: number) {
+    setNavItemDropdownOpen((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }));
+  }
 
   return (
-    <>
-      <AppBar position="fixed" component="nav">
+    <AppBar position="absolute" sx={{ top: 0, left: 0 }}>
+      <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Container maxWidth="xl">
-            <Link
-              href="/home"
-              style={{
-                textDecoration: 'none',
-                color: '#FFFFFF',
-              }}
-            >
-              <Box
-                sx={{
-                  display: { xs: 'none', md: 'flex' },
-                  alignItems: 'center',
-                  gap: '4px',
-                }}
-              >
-                <AppleLogo size={24} />
-                <Typography variant="h6">Logo</Typography>
+          <List sx={{ display: { xs: 'block', md: 'flex' } }}>
+            {navItems.map((item, key) => (
+              <Box component="div" key={key}>
+                {item.subPages ? (
+                  <>
+                    <ListItemButton
+                      onClick={() => handleShowDropdown(item.id)}
+                      alignItems="center"
+                      sx={{ height: '100%' }}
+                    >
+                      <ListItemText primary={item.label} />
+                    </ListItemButton>
+                    <Collapse
+                      in={navItemDropdownOpen[item.id]}
+                      timeout="auto"
+                      unmountOnExit
+                    >
+                      <List
+                        component="div"
+                        disablePadding
+                        sx={{ position: { md: 'absolute' }, top: '64px' }}
+                      >
+                        {item.subPages.map((page, key) => (
+                          <ListItemButton
+                            onClick={() => handleShowDropdown(item.id)}
+                            key={key}
+                          >
+                            <MUILink
+                              component={Link}
+                              href={page.link}
+                              underline="none"
+                              sx={{ color: 'black' }}
+                            >
+                              {page.label}
+                            </MUILink>
+                          </ListItemButton>
+                        ))}
+                      </List>
+                    </Collapse>
+                  </>
+                ) : (
+                  <ListItemButton alignItems="center" sx={{ height: '100%' }}>
+                    <MUILink
+                      component={Link}
+                      href={item.link}
+                      underline="none"
+                      sx={{ color: 'white' }}
+                    >
+                      {item.label}
+                    </MUILink>
+                  </ListItemButton>
+                )}
               </Box>
-            </Link>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer menu"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{
-                mr: 2,
-                display: { md: 'none' },
-              }}
-            >
-              <List size={32} />
-            </IconButton>
-          </Container>
+            ))}
+          </List>
         </Toolbar>
-      </AppBar>
-      <MobileNavbar
-        open={mobileOpen}
-        handleDrawerToggle={handleDrawerToggle}
-        navItems={navItems}
-      />
-    </>
+      </Container>
+    </AppBar>
   );
 };
 
